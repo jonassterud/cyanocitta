30c5bf5f267e31d3c1f26c4e6cafc6aeeea0ee31
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-12-27 00:37:28 +0000

    fix: move declaration of env variables

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,6 +2,10 @@
 
 cargo install tauri-cli --version "^2.0.0-alpha"
 
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export ANDROID_HOME=$HOME/.android
+export NDK_HOME=$ANDROID_HOME/ndk/25.0.8775105
+
 wget https://dl.google.com/android/repository/commandlinetools-linux-8512546_latest.zip -O cmdline-tools.zip
 unzip cmdline-tools.zip
 mkdir ~/.android
@@ -9,3 +13,5 @@ mkdir ~/.android/cmdline-tools
 mv cmdline-tools ~/.android/cmdline-tools/tools
 ~/.android/cmdline-tools/tools/bin/sdkmanager "platforms;android-33" "platform-tools" "ndk;25.0.8775105" "build-tools;33.0.0"
 rm cmdline-tools.zip
+
+cargo tauri android init
