f4713c601532e658e478c37f0326c01767e08dd6
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2023-01-01 19:09:54 +0000

    ci: add test for event message

--- src-tauri/crates/cyanocitta_nostr_tools/src/tests.rs
+++ src-tauri/crates/cyanocitta_nostr_tools/src/tests.rs
@@ -5,3 +5,23 @@ use super::*;
 fn test_profile_new_with_random_keypair() {
     Profile::new_with_random_keypair();
 }
+
+#[test]
+fn test_message_new_event() {
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    let profile = Profile::new_with_random_keypair();
+    Message::Event(
+        message::Event::new(
+            profile.public_key.serialize()[1..].to_vec(),
+            SystemTime::duration_since(&SystemTime::now(), UNIX_EPOCH)
+                .unwrap()
+                .as_secs() as i64,
+            1,
+            vec![],
+            "test".to_string(),
+            profile.secret_key,
+        )
+        .unwrap(),
+    );
+}
