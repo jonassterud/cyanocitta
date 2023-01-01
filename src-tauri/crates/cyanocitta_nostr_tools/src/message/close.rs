e19ebd5ec893cfde3bc9f970e6afcdd7f4f6f563
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2023-01-01 17:11:20 +0000

    feat: close struct

---
+++ src-tauri/crates/cyanocitta_nostr_tools/src/message/close.rs
@@ -0,0 +1,19 @@
+use serde::Serialize;
+
+/// Close.
+#[derive(Debug, Serialize)]
+pub struct Close {
+    /// String representing a subscription.
+    pub subscription_id: String,
+}
+
+impl Close {
+    /// Create [`Close`].
+    /// 
+    /// # Arguments
+    /// 
+    /// * `subscription_id` - string representing a subscription.
+    pub fn new(subscription_id: String) -> Self {
+        Self { subscription_id }
+    }
+}
