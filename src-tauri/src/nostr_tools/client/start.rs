0d3faea78b038758927bbad31f2f56d0dd5109c8
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2023-01-11 23:19:49 +0000

    fix: panic in tasks to see error messages

--- src-tauri/src/nostr_tools/client/start.rs
+++ src-tauri/src/nostr_tools/client/start.rs
@@ -15,7 +15,7 @@ impl super::Client {
             let relays = client.lock().await.relays.clone();
             for relay in relays {
                 // Connect
-                let connection = connect_async(relay).await?.0;
+                let connection = connect_async(relay).await.unwrap().0;
                 let (mut write, mut read) = connection.split();
 
                 // Send messages
@@ -23,15 +23,15 @@ impl super::Client {
                 tokio::spawn(async move {
                     loop {
                         if let Some(message) = client_1.lock().await.pool.pop_front() {
-                            write.send(tungstenite::Message::Text(message)).await?;
+                            write
+                                .send(tungstenite::Message::Text(message))
+                                .await
+                                .unwrap();
                         } else {
                             // TODO: Needed?
                             tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
                         }
                     }
-
-                    #[allow(unreachable_code)]
-                    Result::<(), anyhow::Error>::Ok(())
                 });
 
                 // Read messages
@@ -39,26 +39,21 @@ impl super::Client {
                 tokio::spawn(async move {
                     loop {
                         if let Some(Ok(message)) = read.next().await {
-                            let message = message.to_text()?;
-                            let nostr_message = serde_json::from_str::<Message>(message)?;
+                            let message = message.to_text().unwrap();
+                            let nostr_message = serde_json::from_str::<Message>(message).unwrap();
 
                             match nostr_message {
                                 Message::Event(event) => client_2.lock().await.notes.push(event),
-                                Message::Notice(notice) => return Err(anyhow!(notice)),
-                                _ => return Err(anyhow!("unexpected message from relay")),
+                                Message::Notice(notice) => panic!("{}", notice),
+                                _ => panic!("unexpected message from relay"),
                             }
                         } else {
                             // TODO: Needed?
                             tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
                         }
                     }
-
-                    #[allow(unreachable_code)]
-                    Result::<(), anyhow::Error>::Ok(())
                 });
             }
-
-            Result::<(), anyhow::Error>::Ok(())
         });
     }
 }
