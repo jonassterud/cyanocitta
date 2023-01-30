f9f85ef2685275befe9e941ac3aafb8bdd740f0d
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2023-01-30 10:34:27 +0100

    feat: tauri setup

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,9 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+#[tokio::main]
+pub async fn main() {
+    cyanocitta::AppBuilder::new().run();
+}
