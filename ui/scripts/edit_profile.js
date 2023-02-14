97fe8ccb73b3116cb7afe241ca79c46a9aceb081
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2023-02-14 07:18:26 +0100

    feat: use promises instead of async

--- ui/scripts/edit_profile.js
+++ ui/scripts/edit_profile.js
@@ -7,34 +7,31 @@ window.onload = () => {
     }
 }
 
-function _get_metadata() {
-    return window.__TAURI__.invoke("get_metadata")
-        .then((response) => {
-            return JSON.parse(response);
-        });
-}
-
-function _set_metadata(metadata) {
-    return window.__TAURI__.invoke("set_metadata", { metadata: JSON.stringify(metadata) });
-}
-
-async function load_metadata() {
-    let metadata = await _get_metadata();
-    
-    document.getElementById("picture_preview").src = metadata.picture || "";
-    document.getElementById("picture").value = metadata.picture || "";
-    document.getElementById("name").value = metadata.name || "";
-    document.getElementById("display_name").value = metadata.display_name || "";
-    document.getElementById("about").value = metadata.about || "";
+function load_metadata() {
+    window.__TAURI__.invoke("get_metadata")
+    .then((metadata) => JSON.parse(metadata))
+    .then((metadata) => {
+        document.getElementById("picture_preview").src = metadata.picture || "";
+        document.getElementById("picture").value = metadata.picture || "";
+        document.getElementById("name").value = metadata.name || "";
+        document.getElementById("display_name").value = metadata.display_name || "";
+        document.getElementById("about").value = metadata.about || "";
+    })
+    .catch((error) => {
+        throw error;
+    });
 }
 
 async function save_metadata() {
-    let metadata = {
-        picture: document.getElementById("picture").value || "",
-        name: document.getElementById("name").value || "",
-        display_name: document.getElementById("display_name").value || "",
-        about: document.getElementById("about").value || "",
-    };
-
-    await _set_metadata(metadata);
+    window.__TAURI__.invoke("set_metadata", {
+        metadata: JSON.stringify({
+            picture: document.getElementById("picture").value || "",
+            name: document.getElementById("name").value || "",
+            display_name: document.getElementById("display_name").value || "",
+            about: document.getElementById("about").value || "",
+        })
+    })
+    .catch((error) => {
+        throw error;
+    });
 }
