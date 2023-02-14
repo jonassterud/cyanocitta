c9afa9504d553b298511acda93c793b1319a7221
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2023-02-14 08:11:27 +0100

    fix: return `XOnlyPublicKey`

--- src-tauri/src/commands/pk.rs
+++ src-tauri/src/commands/pk.rs
@@ -5,7 +5,7 @@ use tauri::State;
 #[tauri::command]
 pub async fn get_pk(state: State<'_, ClientState>) -> Result<String, String> {
     let inner = state.0.lock().await;
-    let pk = inner.pk.clone();
+    let pk = XOnlyPublicKey::from_bech32(&inner.pk).map_err(|e| e.to_string())?;
 
-    Ok(pk)
+    Ok(pk.to_string())
 }
