806fd6b7cb07c4907fd2b5d0848f3f92c1b85d18
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2023-02-14 08:55:17 +0100

    feat: use pk on class names to set metadata

--- ui/scripts/view_profile.js
+++ ui/scripts/view_profile.js
@@ -34,12 +34,17 @@ function fill_profile_action_button() {
 
 function load_profile(timeout) {
     let pk = window.localStorage.getItem("pk");
-    let metadata = null;
+    let metadatas = new Map();
     let notes = new Map();
 
     if (pk === null) {
         throw Error("missing public key");
     }
+
+    document.getElementById("profile_name").classList.add(`${pk}_name`);
+    document.getElementById("profile_display_name").classList.add(`${pk}_display_name`);
+    document.getElementById("profile_about").classList.add(`${pk}_about`);
+    document.getElementById("profile_picture").classList.add(`${pk}_picture`);
 
     window.__TAURI__.invoke("get_events_of", {
         filters: [{ authors: [pk], kinds: [0, 1, 2], limit: 5000 }],
@@ -50,10 +55,8 @@ function load_profile(timeout) {
         events.forEach((event) => {
             switch (event.kind) {
                 case 0:
-                    if (metadata === null) {
-                        metadata = JSON.parse(event.content);
-                    }
-    
+                    metadatas.set(event.pubkey, JSON.parse(event.content));
+
                     break;
                 case 1:
                     notes.set(event.id, event);
@@ -70,11 +73,11 @@ function load_profile(timeout) {
         notes.forEach((note) => {
             document.getElementById("notes").innerHTML += `
                 <div class="note">
-                    <img class="picture" id="note_picture" src="media/avatar-default.svg">
+                    <img class="note_picture ${note.pubkey}_picture" src="media/avatar-default.svg">
                     <div>
                         <div>
-                            <span class="display_name" id="note_display_name">Display name</span>
-                            <span class="name" id="note_name">@Username</span>
+                            <span class="note_display_name ${note.pubkey}_display_name">Display name</span>
+                            <span class="note_name ${note.pubkey}_name">@Username</span>
                         </div>
                         <span class="note_content">${note.content}</span>
                     </div>
@@ -83,10 +86,12 @@ function load_profile(timeout) {
         });
     })
     .then(() => {
-        [...document.getElementsByClassName("name")].forEach((e) => e.innerHTML = metadata?.name || pk);
-        [...document.getElementsByClassName("display_name")].forEach((e) => e.innerHTML = metadata?.display_name || pk.substring(0, 8) + "...");
-        [...document.getElementsByClassName("about")].forEach((e) => e.innerHTML = metadata?.about || "");
-        [...document.getElementsByClassName("picture")].forEach((e) => e.src = metadata?.picture || "media/avatar-default.svg");
+        metadatas.forEach((metadata, key) => {
+            [...document.getElementsByClassName(`${key}_name`)].forEach((e) => e.innerHTML = metadata?.name || key);
+            [...document.getElementsByClassName(`${key}_display_name`)].forEach((e) => e.innerHTML = metadata?.display_name || key.substring(0, 8) + "...");
+            [...document.getElementsByClassName(`${key}_about`)].forEach((e) => e.innerHTML = metadata?.about || "");
+            [...document.getElementsByClassName(`${key}_picture`)].forEach((e) => e.src = metadata?.picture || "media/avatar-default.svg");
+        });
     })
     .catch((error) => {
         throw error;
