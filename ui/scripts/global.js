61198b3fe05cc535432ab19983e9d2b95f525adb
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2023-02-15 07:12:30 +0000

    feat: global footer

--- ui/scripts/global.js
+++ ui/scripts/global.js
@@ -1,3 +1,13 @@
+function set_viewing_pk_to_my_pk() {
+    window.__TAURI__.invoke("get_my_pk")
+        .then((my_pk) => {
+            window.localStorage.setItem("viewing_pk", my_pk);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
 /*
 function get_metadata_from_pk(pk, timeout) {
     return window.__TAURI__.invoke("get_events_of", {
--- ui/scripts/index.js
+++
@@ -1,9 +0,0 @@
-function set_viewing_pk_to_my_pk() {
-    window.__TAURI__.invoke("get_my_pk")
-        .then((my_pk) => {
-            window.localStorage.setItem("viewing_pk", my_pk);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-}
