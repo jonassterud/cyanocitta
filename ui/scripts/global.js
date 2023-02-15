72ab5ae9413c1346e9dde8a42a84b2ff327a68bc
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2023-02-15 12:03:21 +0100

    fix: default metadata

--- ui/scripts/global.js
+++ ui/scripts/global.js
@@ -17,8 +17,8 @@ function get_notes_html(notes) {
                 <img class="note_picture ${notes[key].pubkey}_picture" src="media/avatar-default.svg">
                 <div>
                     <div>
-                        <span class="note_display_name ${notes[key].pubkey}_display_name">Display name</span>
-                        <span class="note_name ${notes[key].pubkey}_name">@Username</span>
+                        <span class="note_display_name ${notes[key].pubkey}_display_name">${key.substring(0, 8) + "..."}</span>
+                        <span class="note_name ${notes[key].pubkey}_name">${key}</span>
                     </div>
                     <span class="note_content">${notes[key].content}</span>
                 </div>
@@ -31,9 +31,9 @@ function get_notes_html(notes) {
 
 function display_metadata(metadata) {
     for (let key in metadata) {
-        [...document.getElementsByClassName(`${key}_name`)].forEach((e) => e.innerHTML = metadata[key].name || key);
-        [...document.getElementsByClassName(`${key}_display_name`)].forEach((e) => e.innerHTML = metadata[key].display_name || key.substring(0, 8) + "...");
-        [...document.getElementsByClassName(`${key}_about`)].forEach((e) => e.innerHTML = metadata[key].about || "");
-        [...document.getElementsByClassName(`${key}_picture`)].forEach((e) => e.src = metadata[key].picture || "media/avatar-default.svg");
+        [...document.getElementsByClassName(`${key}_name`)].forEach((e) => e.innerHTML = metadata[key]?.name || key);
+        [...document.getElementsByClassName(`${key}_display_name`)].forEach((e) => e.innerHTML = metadata[key]?.display_name || key.substring(0, 8) + "...");
+        [...document.getElementsByClassName(`${key}_about`)].forEach((e) => e.innerHTML = metadata[key]?.about || "");
+        [...document.getElementsByClassName(`${key}_picture`)].forEach((e) => e.src = metadata[key]?.picture || "media/avatar-default.svg");
     }
 }
