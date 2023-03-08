de6943b1e578b4852130c18f3b7cc0a84cbed973
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2023-03-08 18:54:05 +0000

    feat: app state structure

--- crates/cyanocitta/src/lib.rs
+++ crates/cyanocitta/src/lib.rs
@@ -1,6 +1,11 @@
+mod app_state;
+
+use app_state::AppState;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .manage(AppState::new())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
