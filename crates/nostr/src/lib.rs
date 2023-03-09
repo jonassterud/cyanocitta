53ee67b19dfb917c004e8171c09c8fefffd0fae1
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2023-03-09 02:27:03 +0000

    fix(nostr): add metadata to prelude

--- crates/nostr/src/lib.rs
+++ crates/nostr/src/lib.rs
@@ -4,6 +4,7 @@ mod types;
 pub use prelude::*;
 pub mod prelude {
     pub use super::client::{Client, Relay};
+    pub use super::types::Metadata;
     pub use super::types::{ClientMessage, RelayMessage, SubscriptionId};
     pub use super::types::{Event, EventContent, EventId, EventKind, EventSig, EventTag, EventTimestamp};
     pub use super::types::{Filter, FilterTags};
